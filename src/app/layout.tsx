e2bfa05a101635c046023a025551f821c3e68b24
AntoineSirot <sirot_antoine@bbox.fr>
2023-12-02 16:57:09 +0100

    chore: Init of Tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import '@rainbow-me/rainbowkit/styles.css'
 import { Providers } from './providers'
 
+// import global.css here
+import './../../global.css'
+
 export const metadata = {
   title: 'wagmi',
 }
