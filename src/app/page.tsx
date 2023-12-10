153669cf525515f2c77916d3aa9fc9f3daffd7d8
AntoineSirot <sirot_antoine@bbox.fr>
2023-12-10 15:50:44 +0100

    chore: page and wagmi.ts updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,43 @@
-import { Account } from '../components/Account'
-import { Balance } from '../components/Balance'
-import { BlockNumber } from '../components/BlockNumber'
 import { ConnectButton } from '../components/ConnectButton'
 import { Connected } from '../components/Connected'
-import { NetworkSwitcher } from '../components/NetworkSwitcher'
-import { ReadContract } from '../components/ReadContract'
-import { ReadContracts } from '../components/ReadContracts'
-import { ReadContractsInfinite } from '../components/ReadContractsInfinite'
-import { SendTransaction } from '../components/SendTransaction'
-import { SendTransactionPrepared } from '../components/SendTransactionPrepared'
-import { SignMessage } from '../components/SignMessage'
-import { SignTypedData } from '../components/SignTypedData'
-import { Token } from '../components/Token'
-import { WatchContractEvents } from '../components/WatchContractEvents'
-import { WatchPendingTransactions } from '../components/WatchPendingTransactions'
-import { WriteContract } from '../components/WriteContract'
-import { WriteContractPrepared } from '../components/WriteContractPrepared'
+import { Swap } from '../components/Swap'
+import { Tokens } from '../components/Tokens'
+import { TokenDashboard } from '../components/TokenDashboard'
+import React from 'react'
 
-export function Page() {
+const Page: React.FC = () => {
   return (
     <>
-      <h1>wagmi + RainbowKit + Next.js</h1>
 
-      <ConnectButton />
+      <div className="text-center pt-6 pb-6 font-bold text-2xl text-green-900">
+        Oracle and Indexers Application
+      </div>
+
+      <div className="flex justify-center items-center pb-4">
+        <div className="display-flex">
+          <ConnectButton />
+        </div>
+      </div>
+
 
       <Connected>
         <hr />
-        <h2>Network</h2>
-        <NetworkSwitcher />
+        <div className="text-center pt-12 pb-12 font-bold text-2xl text-blue-900">
+          Swap ETH and StableCoin with chainlink&apos;s oracle&apos;s price for ETH
+        </div>
+        <Tokens />
         <br />
+        <h2 className="text-center pb-6 font-bold text-blue-900">Swap</h2>
+        <Swap />
         <hr />
-        <h2>Account</h2>
-        <Account />
+        <div className="text-center pt-12 pb-6 font-bold text-2xl text-green-900">
+          Get the 3 biggest whales of a Sushiswap pool with TheGraph
+        </div>
+
+        <TokenDashboard />
         <br />
-        <hr />
-        <h2>Balance</h2>
-        <Balance />
-        <br />
-        <hr />
-        <h2>Block Number</h2>
-        <BlockNumber />
-        <br />
-        <hr />
-        <h2>Read Contract</h2>
-        <ReadContract />
-        <br />
-        <hr />
-        <h2>Read Contracts</h2>
-        <ReadContracts />
-        <br />
-        <hr />
-        <h2>Read Contracts Infinite</h2>
-        <ReadContractsInfinite />
-        <br />
-        <hr />
-        <h2>Send Transaction</h2>
-        <SendTransaction />
-        <br />
-        <hr />
-        <h2>Send Transaction (Prepared)</h2>
-        <SendTransactionPrepared />
-        <br />
-        <hr />
-        <h2>Sign Message</h2>
-        <SignMessage />
-        <br />
-        <hr />
-        <h2>Sign Typed Data</h2>
-        <SignTypedData />
-        <br />
-        <hr />
-        <h2>Token</h2>
-        <Token />
-        <br />
-        <hr />
-        <h2>Watch Contract Events</h2>
-        <WatchContractEvents />
-        <br />
-        <hr />
-        <h2>Watch Pending Transactions</h2>
-        <WatchPendingTransactions />
-        <br />
-        <hr />
-        <h2>Write Contract</h2>
-        <WriteContract />
-        <br />
-        <hr />
-        <h2>Write Contract (Prepared)</h2>
-        <WriteContractPrepared />
       </Connected>
+
     </>
   )
 }
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,19 +1,22 @@
+import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { configureChains, createConfig } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
+import { sepolia, mainnet } from 'wagmi/chains'
+
 import { publicProvider } from 'wagmi/providers/public'
 
-const walletConnectProjectId = 'd7ed93e3f4ae7e19df8fc763018ef107'
+const walletConnectProjectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || ''
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
+  [sepolia],
   [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "" }),
     publicProvider(),
   ],
 )
 
 const { connectors } = getDefaultWallets({
-  appName: 'My wagmi + RainbowKit App',
+  appName: 'Voting System',
   chains,
   projectId: walletConnectProjectId,
 })
